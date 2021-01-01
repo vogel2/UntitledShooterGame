0a6f9ba52258014cf7103fd817c4053a12db4502
vogel2 <48958459+vogel2@users.noreply.github.com>
2021-01-01 14:36:38 +0200

    Initial commit

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+        Debug.Log("Connected to Master!");
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined Lobby!");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
