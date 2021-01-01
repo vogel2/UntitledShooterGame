5f85107be1c1ef747f0f06380dfb89575d402656
vogel2 <48958459+vogel2@users.noreply.github.com>
2021-01-01 15:13:19 +0200

    3 - Launcher Update 2
    Added:
    "Connecting to Master.." message log

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -8,7 +8,7 @@ public class Launcher : MonoBehaviourPunCallbacks
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Connecting to Master..")
+        Debug.Log("Connecting to Master..");
         PhotonNetwork.ConnectUsingSettings();
     }
 
