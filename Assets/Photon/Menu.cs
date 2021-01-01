1b834179a0463298ae9bb48c5da720ac5187dee4
vogel2 <48958459+vogel2@users.noreply.github.com>
2021-01-01 20:02:06 +0200

    5 - Menu and Menu Manager
    -Added Menu and Menu Manager script

---
+++ Assets/Photon/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public string menuName;
+    [HideInInspector] public bool IsOpen;
+    public void Open()
+    {
+        IsOpen = true;
+        gameObject.SetActive(true);
+    }
+
+    public void Close()
+    {
+        IsOpen = false;
+        gameObject.SetActive(false);
+    }
+}
